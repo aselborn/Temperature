4564981d51ff00d57f4584a283e96b76d74e9957
Selborn, Anders <anders.selborn@raysafe.com>
2022-09-05 11:53:47 +0200

    json-obj-station

--- Pages/Station.cshtml.cs
+++ Pages/Station.cshtml.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.VisualBasic.CompilerServices;
+using TempCoreApi.Models;
 
 namespace Temperature.Pages
 {
-    public class StationModel : PageModel
+    public class StationModel : PageModel, IBootstrap
     {
         private IBootstrap bootstrap;
         private void GetServiceManual()
@@ -22,16 +24,19 @@ namespace Temperature.Pages
         {
             //get stations!
 
+            using var client = new HttpClient();
+            string path = string.Concat(Setup().ApiPath, "/station");
+            var content = await client.GetStringAsync(path);
 
+            var info = JsonSerializer.Deserialize<StationInformation>(content);
+        }
+
+        public ApplicationConfig Setup()
+        {
             var services = this.HttpContext.RequestServices;
             var boot = (IBootstrap)services.GetService(typeof(IBootstrap));
 
-            using var client = new HttpClient();
-            string path = string.Concat(boot.Setup().ApiPath, "/station");
-            var content = await client.GetStringAsync(path);
-
-            var ps = content;
-
+            return boot.Setup();
         }
     }
 }
