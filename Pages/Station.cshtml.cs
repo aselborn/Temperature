d56f561f82e57bd63ce7b9ecd26493fd15344eae
Selborn, Anders <anders.selborn@raysafe.com>
2022-09-08 11:07:17 +0200

    select från api.

--- Pages/Station.cshtml.cs
+++ Pages/Station.cshtml.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.VisualBasic.CompilerServices;
+using Newtonsoft.Json;
 using TempCoreApi.Models;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Temperature.Pages
 {
     public class StationModel : PageModel, IBootstrap
     {
+        [ViewData]
+        public List<StationInformation> SmhiStationInformations { get; set; }
+
         private IBootstrap bootstrap;
         private void GetServiceManual()
         {
@@ -28,7 +32,8 @@ namespace Temperature.Pages
             string path = string.Concat(Setup().ApiPath, "/station");
             var content = await client.GetStringAsync(path);
 
-            var info = JsonSerializer.Deserialize<StationInformation>(content);
+            SmhiStationInformations= JsonConvert.DeserializeObject<List<StationInformation>>(content);
+
         }
 
         public ApplicationConfig Setup()
